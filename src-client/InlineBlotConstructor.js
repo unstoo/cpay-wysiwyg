2cda61cbca19891d9bc918460734b95941a488b1
Yevgeny <unstoo@gmail.com>
2018-05-26 18:09:34 +0300

    fixed tab size

--- src-client/InlineBlotConstructor.js
+++ src-client/InlineBlotConstructor.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const InlineBlotConstructor = ({ buttonName, formatName }) => {
 
-    const applyFormat = (e, format = formatName) => {
-            const appliedFormats = quill.getFormat()
-            if (!appliedFormats[format]) {
-                quill.format(format, true)
-            } else {
-                quill.format(format, false)
-            }
-        }
+  const applyFormat = (e, format = formatName) => {
+    const appliedFormats = quill.getFormat()
+    if (!appliedFormats[format]) {
+        quill.format(format, true)
+    } else {
+        quill.format(format, false)
+    }
+  }
 
-    return <button onClick={applyFormat}>
-        {buttonName}
-    </button>
+  return <button onClick={applyFormat}>
+      {buttonName}
+  </button>
 }
 
 export default InlineBlotConstructor
